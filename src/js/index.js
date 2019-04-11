7bdef0b3a5875027165e791e916487810705c27f
Filip Š <filip.stamcar@hotmail.com>
2019-04-11 17:03:17 +0200

    Add example site

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+'use struct'
+
+const ZeroFrame = require('./ZeroFrame')
+
+class Page extends ZeroFrame {
+  setSiteInfo (siteInfo) {
+    const out = document.getElementById('out')
+
+    out.innerHTML =
+      '<h1>Site Details</h1>' +
+      '- Page address: <span class="details">' + siteInfo.address + '</span>' +
+      '<br>- Peers: <span class="details">' + siteInfo.peers + '</span>' +
+      '<br>- Size: <span class="details">' + siteInfo.settings.size + '</span>' +
+      '<br>- Modified: <span class="details">' + (new Date(siteInfo.content.modified * 1000)) + '</span>'
+  }
+
+  onOpenWebsocket () {
+    this.cmd('siteInfo', [], function (siteInfo) {
+      page.setSiteInfo(siteInfo)
+    })
+  }
+
+  onRequest (cmd, message) {
+    if (cmd === 'setSiteInfo') {
+      this.setSiteInfo(message.params)
+    } else {
+      this.log('Unknown incoming message:', cmd)
+    }
+  }
+}
+
+const page = new Page()
